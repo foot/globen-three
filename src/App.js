96f8357c37c130be392406f5ba251c20beb261e7
Simon Howe <footless@gmail.com>
2021-02-28 22:22:00 +0100

    Rendering vanilla lat/lng lines
    ...next comes data.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { chunk } from "lodash";
+import "./App.css";
+import { Globe } from "./Globe";
 
 function App() {
+  useEffect(() => {
+    window
+      .fetch("./data/population909500.json")
+      .then((res) => res.json())
+      .then(([a, b, [year, data]]) => console.log(chunk(data, 3)));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Globe />
     </div>
   );
 }
---
+++ src/Globe.js
@@ -0,0 +1,106 @@
+import React, { useMemo, useEffect, useRef, useState } from "react";
+import { Canvas, useFrame, extend, useThree } from "react-three-fiber";
+import { range } from "lodash";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+extend({ OrbitControls });
+
+function toPoint(lat, lng, u) {
+  const phi = (90 - lat) * (Math.PI / 180);
+  const theta = (180 - lng) * (Math.PI / 180);
+  const x = u * Math.sin(phi) * Math.cos(theta);
+  const y = u * Math.cos(phi);
+  const z = u * Math.sin(phi) * Math.sin(theta);
+  return [x, y, z];
+}
+
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef();
+
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame(() => {
+    mesh.current.rotation.y += 0.01;
+  });
+
+  const step = -90 / 10;
+  const lats = range(90, -90 + step, step);
+  const lngs = range(180, -180 + step, step);
+
+  const lines = useMemo(
+    () =>
+      lats.map((lat) => {
+        const positions = new Float32Array(
+          lngs.flatMap((lng) => toPoint(lat, lng, 2))
+        );
+        return (
+          <line key={`${lat}`}>
+            <bufferGeometry attach="geometry">
+              <bufferAttribute
+                attachObject={["attributes", "position"]}
+                count={positions.length / 3}
+                array={positions}
+                itemSize={3}
+              />
+            </bufferGeometry>
+            <lineBasicMaterial attach="material" color="black" />
+          </line>
+        );
+      }),
+    []
+  );
+
+  return (
+    <>
+      {lines}
+      <mesh
+        {...props}
+        ref={mesh}
+        scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+        onClick={(event) => setActive(!active)}
+        onPointerOver={(event) => setHover(true)}
+        onPointerOut={(event) => setHover(false)}
+      >
+        <sphereBufferGeometry args={[1, 12, 12]} />
+        <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+      </mesh>
+    </>
+  );
+}
+
+const camContext = React.createContext();
+function Controls({ children }) {
+  const { gl, camera, invalidate } = useThree();
+  const api = useState(true);
+  const ref = useRef();
+  useEffect(() => {
+    const current = ref.current;
+    const handler = current.addEventListener("change", invalidate);
+    return () => current.removeEventListener("change", handler);
+  }, [invalidate]);
+
+  return (
+    <>
+      <orbitControls
+        ref={ref}
+        args={[camera, gl.domElement]}
+        enableDamping
+        enabled={api[0]}
+      />
+      <camContext.Provider value={api}>{children}</camContext.Provider>
+    </>
+  );
+}
+
+export const Globe = () => (
+  <Canvas raycaster={{ params: { Line: { threshold: 5 } } }}>
+    <ambientLight />
+    <pointLight position={[10, 10, 10]} />
+    <Controls>
+      <Box position={[0, 0, 0]} />
+    </Controls>
+  </Canvas>
+);
